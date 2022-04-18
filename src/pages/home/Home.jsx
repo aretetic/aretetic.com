40eec82bef207ee65f9e766954fd1454d737a5f8
Ian Terry <ian@aretetic.com>
2022-04-18 12:17:09 -0700

    hello?

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import { Grid } from '@mui/material'
 import './Home.css'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <Grid container spacing={2} justifyContent='center' alignItems='center'>
+        <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+            <div width="100%" height="100%">
+              <video autoplay loop muted id="backgroundVideo">
+                <source src="public/video/Pexels Videos 2711127.mp4" type='video/mp4' />
+              </video>
+            </div>
+        </Grid>
+    </Grid>
   )
 }
 
