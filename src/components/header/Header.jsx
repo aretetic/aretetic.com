c64dbadc100ef6838d174c0ab41e9dad0687ebf8
Ian Terry <ian@aretetic.com>
2022-04-18 13:26:17 -0700

    adding mui

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,18 @@
+import { Grid } from '@mui/material'
 import React from 'react'
 import './Header.css'
+import logo from './logo_withSloganWhite.png'
 
 const Header = () => {
   return (
     <>
-    <div>Header</div>
+    <Grid container spacing={2}>
+      <Grid item xs={12}>
+        <div>
+          <image src={logo} />
+          </div>
+      </Grid>
+    </Grid>
     </>
   )
 }
