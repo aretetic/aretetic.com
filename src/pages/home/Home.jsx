315500925d69764e16433d0fd2723adc0438ff59
Ian Terry <ian@aretetic.com>
2022-04-18 12:53:53 -0700

    moving video location

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,19 +1,17 @@
 import React from 'react'
-import { Grid } from '@mui/material'
+import { } from '@mui/material'
 import './Home.css'
+import backgroundVideo from './PexelsVideos2711127.mp4'
 
 const Home = () => {
   return (
     <>
     <h2> hello?</h2>
-    <Grid container spacing={2} justifyContent='center' alignItems='center'>
-        <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
-              <video autoplay loop muted id="video">
-                <source src='../public/video/PexelsVideos2711127.mp4' type='video/mp4' />
+   
+              <video autoPlay loop muted id='video'>
+                <source src= {backgroundVideo} type='video/mp4' />
               </video>
               <h2>this is some text</h2>
-        </Grid>
-    </Grid>
     </>
   )
 }
