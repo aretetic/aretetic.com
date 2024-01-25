d0908467d7555d776feb6579b56d883fb82851fc
aretetic <90521448+aretetic@users.noreply.github.com>
2024-01-25 10:59:13 -0800

    moving scrolltotop into a helper component rather than app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { Routes, Route, useLocation } from 'react-router-dom';
 import * as React from 'react';
-import { useEffect } from 'react';
 import './App.css';
 import { Navigate } from 'react-router-dom';
 import Header from './components/header/Header';
@@ -14,16 +13,8 @@ import SoChoice from './pages/products/sochoice/SoChoice'
 import Services from './pages/services/Services';
 import Contact from './pages/contact/Contact';
 import Footer from './components/footer/Footer';
+import ScrollToTop from 'components/helpers/ScrollToTop';
 
-function ScrollToTop() {
-  const location = useLocation();
-
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [location.pathname]);
-
-  return null;
-}
 
 function App() {
   const location = useLocation();
@@ -37,9 +28,9 @@ function App() {
         <Route path='/about-aretetic-solutions' element={<About />} />
         <Route path='/aretetic-products' element={<Products />} />
         <Route path='/aretetic-products/community-driven-innovation' element={<CDI />} />
-		<Route path='/aretetic-products/digital-cabinet' element={<DigitalCabinet />} />
-		<Route path='/aretetic-products/priopeak' element={<PrioPeak />} />
-		<Route path='/aretetic-products/sochoice' element={<SoChoice />} />
+        <Route path='/aretetic-products/digital-cabinet' element={<DigitalCabinet />} />
+        <Route path='/aretetic-products/priopeak' element={<PrioPeak />} />
+        <Route path='/aretetic-products/sochoice' element={<SoChoice />} />
         <Route path='/aretetic-solutions-services' element={<Services />} />
         <Route path='/contact-us' element={<Contact />} />
         <Route path='*' element={<Navigate to='/' replace />} />
